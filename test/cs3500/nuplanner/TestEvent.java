761b4b8fe65717fe93ea516bb5961ce190ca0a02
Brandon <lu.bra@northeastern.edu>
2024-03-14 13:32:03 -0400

    Added all relevant tests to TestEvent, need fixes to NUEvent and once over on tests

--- test/cs3500/nuplanner/TestEvent.java
+++ test/cs3500/nuplanner/TestEvent.java
@@ -10,9 +10,12 @@ import cs3500.nuplanner.model.hw05.DaysOfTheWeek;
 import cs3500.nuplanner.model.hw05.Event;
 import cs3500.nuplanner.model.hw05.NUEvent;
 
+import static cs3500.nuplanner.model.hw05.DaysOfTheWeek.FRIDAY;
 import static cs3500.nuplanner.model.hw05.DaysOfTheWeek.MONDAY;
+import static cs3500.nuplanner.model.hw05.DaysOfTheWeek.SATURDAY;
 import static cs3500.nuplanner.model.hw05.DaysOfTheWeek.SUNDAY;
 import static cs3500.nuplanner.model.hw05.DaysOfTheWeek.TUESDAY;
+import static cs3500.nuplanner.model.hw05.DaysOfTheWeek.WEDNESDAY;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThrows;
@@ -28,16 +31,16 @@ public class TestEvent {
 
     Event event1 = new NUEvent(new ArrayList<>(List.of("Mo", "Ko", "Jo")),
             "Tennis", "Krentzman Quad", true,
-            DaysOfTheWeek.WEDNESDAY, 1000, DaysOfTheWeek.WEDNESDAY, 1100);
+            WEDNESDAY, 1000, WEDNESDAY, 1100);
 
     // comparing elements within Lists
     assertEquals(new ArrayList<>(List.of("Mo", "Ko", "Jo")), event1.eventInvitees());
     assertEquals("Tennis", event1.name());
     assertEquals("Krentzman Quad", event1.location());
     assertTrue(event1.isOnline());
-    assertEquals(DaysOfTheWeek.WEDNESDAY, event1.startDay());
+    assertEquals(WEDNESDAY, event1.startDay());
     assertEquals(1000, event1.startTime());
-    assertEquals(DaysOfTheWeek.WEDNESDAY, event1.endDay());
+    assertEquals(WEDNESDAY, event1.endDay());
     assertEquals(1100, event1.endTime());
     assertEquals("Mo", event1.host());
   }
@@ -47,29 +50,36 @@ public class TestEvent {
 
     Event event1 = new NUEvent(new ArrayList<>(List.of("Mo", "Ko", "Jo")),
             "Tennis", "Krentzman Quad", true,
-            DaysOfTheWeek.WEDNESDAY, 1000, DaysOfTheWeek.WEDNESDAY, 1100);
+            WEDNESDAY, 1000, WEDNESDAY, 1100);
 
     Event event2 = new NUEvent(new ArrayList<>(List.of("Mo", "Ko", "Jo")),
             "Basketball", "Krentzman Quad", true,
-            DaysOfTheWeek.WEDNESDAY, 1100, DaysOfTheWeek.WEDNESDAY, 1200);
+            WEDNESDAY, 1100, WEDNESDAY, 1200);
 
     Event event3 = new NUEvent(new ArrayList<>(List.of("Mo", "Ko", "Jo")),
             "Baseball", "Krentzman Quad", true,
-            DaysOfTheWeek.WEDNESDAY, 1200, DaysOfTheWeek.WEDNESDAY, 1300);
+            WEDNESDAY, 1200, WEDNESDAY, 1300);
+  }
+
+  @Test
+  public void checkEventValidTimeSpanCanGoToSecondWeek() {
+    Event twoWeekEvent = new NUEvent(new ArrayList<>(List.of("Mo", "Ko", "Jo")),
+            "Soccer", "Carter Field", false,
+            WEDNESDAY, 1000, SUNDAY, 1200);
   }
 
   @Test
   public void checkEventValidTimeSpanLongestPossibleEvent() {
     Event longEvent = new NUEvent(new ArrayList<>(List.of("Mo", "Ko", "Jo")),
             "Soccer", "Carter Field", false,
-            DaysOfTheWeek.WEDNESDAY, 0, DaysOfTheWeek.TUESDAY, 2359);
+            WEDNESDAY, 0, DaysOfTheWeek.TUESDAY, 2359);
   }
 
   @Test
   public void checkEventInvalidTimeSpanLongerThanOneWeek() { //TODO: How to test longer than one week?
     Event longEvent = new NUEvent(new ArrayList<>(List.of("Mo", "Ko", "Jo")),
             "Soccer", "Carter Field", false,
-            DaysOfTheWeek.WEDNESDAY, 1200, DaysOfTheWeek.WEDNESDAY, 1201);
+            WEDNESDAY, 1200, WEDNESDAY, 1201);
   }
 
   @Test
@@ -78,7 +88,7 @@ public class TestEvent {
     try {
       Event event1 = new NUEvent(new ArrayList<>(List.of("Mo", "Ko", "Jo")),
               "Tennis", "Krentzman Quad", true,
-              DaysOfTheWeek.WEDNESDAY, 962, DaysOfTheWeek.WEDNESDAY, 1100);
+              WEDNESDAY, 962, WEDNESDAY, 1100);
       Assert.fail("Should not be able to construct event with invalid military time... ");
     } catch (IllegalArgumentException ignore) {
       //do nothing
@@ -90,7 +100,7 @@ public class TestEvent {
     try {
       Event event1 = new NUEvent(new ArrayList<>(List.of("Mo", "Ko", "Jo")),
               "Tennis", "Krentzman Quad", true,
-              DaysOfTheWeek.WEDNESDAY, 1000, DaysOfTheWeek.WEDNESDAY, 1192);
+              WEDNESDAY, 1000, WEDNESDAY, 1192);
       Assert.fail("Should not be able to construct event with invalid military time... ");
     } catch (IllegalArgumentException ignore) {
       //do nothing
@@ -102,7 +112,7 @@ public class TestEvent {
     try {
       Event event1 = new NUEvent(new ArrayList<String>(List.of("Mo", "Ko", "Jo")),
               "Tennis", "Krentzman Quad", true,
-              DaysOfTheWeek.WEDNESDAY, 3612, DaysOfTheWeek.WEDNESDAY, 1100);
+              WEDNESDAY, 3612, WEDNESDAY, 1100);
       Assert.fail("Should not be able to construct event with invalid military time... ");
     } catch (IllegalArgumentException ignore) {
       //do nothing
@@ -114,7 +124,7 @@ public class TestEvent {
     try {
       Event event1 = new NUEvent(new ArrayList<String>(List.of("Mo", "Ko", "Jo")),
               "Tennis", "Krentzman Quad", true,
-              DaysOfTheWeek.WEDNESDAY, 1000, DaysOfTheWeek.WEDNESDAY, 4812);
+              WEDNESDAY, 1000, WEDNESDAY, 4812);
       Assert.fail("Should not be able to construct event with invalid military time... ");
     } catch (IllegalArgumentException ignore) {
       //do nothing
@@ -126,7 +136,7 @@ public class TestEvent {
     try {
       Event event1 = new NUEvent(new ArrayList<String>(List.of("Mo", "Ko", "Jo")),
               "Tennis", "Krentzman Quad", true,
-              DaysOfTheWeek.WEDNESDAY, 1100, DaysOfTheWeek.WEDNESDAY, 1100);
+              WEDNESDAY, 1100, WEDNESDAY, 1100);
       Assert.fail("Should not be able to construct event with invalid time span... ");
     } catch (IllegalArgumentException ignore) {
       //do nothing
@@ -138,7 +148,7 @@ public class TestEvent {
     try {
       Event invalidEvent = new NUEvent(new ArrayList<>(List.of("Mo", "Ko", "Jo")),
               null, "Carter Field", false,
-              DaysOfTheWeek.WEDNESDAY, 1100, DaysOfTheWeek.WEDNESDAY, 1200);
+              WEDNESDAY, 1100, WEDNESDAY, 1200);
       Assert.fail("Event cannot be init. with null argument(s)... ");
     } catch (IllegalArgumentException ignore) {
       //do nothing
@@ -150,7 +160,7 @@ public class TestEvent {
     try {
       Event invalidEvent = new NUEvent(new ArrayList<>(List.of("Mo", "Ko", "Jo")),
               "Soccer", null, false,
-              DaysOfTheWeek.WEDNESDAY, 1100, DaysOfTheWeek.WEDNESDAY, 1200);
+              WEDNESDAY, 1100, WEDNESDAY, 1200);
       Assert.fail("Event cannot be init. with null argument(s)... ");
     } catch (IllegalArgumentException ignore) {
       //do nothing
@@ -162,7 +172,7 @@ public class TestEvent {
     try {
       Event invalidEvent = new NUEvent(new ArrayList<>(List.of("Mo", "Ko", "Jo")),
               "Soccer", "Carter Field", false,
-              null, 1100, DaysOfTheWeek.WEDNESDAY, 1200);
+              null, 1100, WEDNESDAY, 1200);
       Assert.fail("Event cannot be init. with null argument(s)... ");
     } catch (IllegalArgumentException ignore) {
       //do nothing
@@ -174,7 +184,7 @@ public class TestEvent {
     try {
       Event invalidEvent = new NUEvent(new ArrayList<>(List.of("Mo", "Ko", "Jo")),
               "Soccer", "Carter Field", false,
-              DaysOfTheWeek.WEDNESDAY, 1100, null, 1200);
+              WEDNESDAY, 1100, null, 1200);
       Assert.fail("Event cannot be init. with null argument(s)... ");
     } catch (IllegalArgumentException ignore) {
       //do nothing
@@ -186,7 +196,7 @@ public class TestEvent {
     try {
       Event invalidEvent = new NUEvent(null,
               "Soccer", "Carter Field", false,
-              DaysOfTheWeek.WEDNESDAY, 1100, null, 1200);
+              WEDNESDAY, 1100, null, 1200);
       Assert.fail("Event cannot be init. with null argument(s)... ");
     } catch (IllegalArgumentException ignore) {
       //do nothing
@@ -198,7 +208,7 @@ public class TestEvent {
     try {
       Event invalidEvent = new NUEvent(new ArrayList<>(List.of()),
               "Soccer", "Carter Field", false,
-              DaysOfTheWeek.WEDNESDAY, 1100, null, 1200);
+              WEDNESDAY, 1100, null, 1200);
       Assert.fail("Event cannot be init. with no invitees... ");
     } catch (IllegalArgumentException ignore) {
       //do nothing
@@ -286,9 +296,10 @@ public class TestEvent {
   public void testInValidEventModificationUpdateStartDayTooLong() {
     Event event = new NUEvent(new ArrayList<>(List.of("John", "Ella")),
             "Soccer", "Carter Field", false,
-            MONDAY, 1100, TUESDAY, 1200); //TODO: Should be the next week
-    assertEquals(MONDAY, event.startDay());
-    assertThrows(NullPointerException.class, () -> event.updateStartDay(null));
+            FRIDAY, 1200, FRIDAY, 1159);
+    assertEquals(FRIDAY, event.startDay());
+    event.updateStartDay(SATURDAY);
+    assertEquals(SATURDAY, event.startDay());
   }
 
   @Test
@@ -302,6 +313,25 @@ public class TestEvent {
   }
 
   @Test
+  public void testInValidEventModificationUpdateEndDayNull() {
+    Event event = new NUEvent(new ArrayList<>(List.of("John", "Ella")),
+            "Soccer", "Carter Field", false,
+            MONDAY, 1100, MONDAY, 1200);
+    assertEquals(MONDAY, event.endDay());
+    assertThrows(NullPointerException.class, () -> event.updateEndDay(null));
+  }
+
+  @Test
+  public void testInValidEventModificationUpdateEndDayTooLong() {
+    Event event = new NUEvent(new ArrayList<>(List.of("John", "Ella")),
+            "Soccer", "Carter Field", false,
+            FRIDAY, 1200, FRIDAY, 1159);
+    assertEquals(FRIDAY, event.endDay());
+    event.updateEndDay(SATURDAY);
+    assertEquals(SATURDAY, event.endDay());
+  }
+
+  @Test
   public void testValidEventModificationUpdateStartTime() {
     Event event = new NUEvent(new ArrayList<>(List.of("John", "Ella")),
             "Soccer", "Carter Field", false,
@@ -312,6 +342,18 @@ public class TestEvent {
   }
 
   @Test
+  public void testInvalidEventModificationUpdateStartTime() {
+    Event event = new NUEvent(new ArrayList<>(List.of("John", "Ella")),
+            "Soccer", "Carter Field", false,
+            MONDAY, 1100, MONDAY, 1130);
+    assertEquals(1100, event.startTime());
+    event.updateStartTime(1200);
+    assertEquals(1200, event.startTime());
+    assertEquals(1130, event.endTime());
+    assertThrows(IllegalArgumentException.class, () -> event.updateStartTime(1200));
+  }
+
+  @Test
   public void testValidEventModificationUpdateEndTime() {
     Event event = new NUEvent(new ArrayList<>(List.of("John", "Ella")),
             "Soccer", "Carter Field", false,
@@ -319,6 +361,18 @@ public class TestEvent {
     assertEquals(1200, event.endTime());
     event.updateEndTime(1230);
     assertEquals(1230, event.endTime());
+  }
+
+  @Test
+  public void testInvalidEventModificationUpdateEndTime() {
+    Event event = new NUEvent(new ArrayList<>(List.of("John", "Ella")),
+            "Soccer", "Carter Field", false,
+            MONDAY, 1100, MONDAY, 1130);
+    assertEquals(1100, event.startTime());
+    event.updateEndTime(1000);
+    assertEquals(1100, event.startTime());
+    assertEquals(1000, event.endTime());
+    assertThrows(IllegalArgumentException.class, () -> event.updateEndTime(1000));
   }
 
   @Test
@@ -334,6 +388,28 @@ public class TestEvent {
   }
 
   @Test
+  public void testInvalidEventModificationRemoveInvitee() { //TODO: IDK if this should pass
+    Event event = new NUEvent(new ArrayList<>(List.of("Ella")),
+            "Soccer", "Carter Field", false,
+            MONDAY, 1100, MONDAY, 1200);
+    List<String> original = new ArrayList<>(List.of("Ella"));
+    assertEquals(original, event.eventInvitees());
+    event.removeInvitee("Ella");
+    List<String> updated = new ArrayList<>(List.of());
+    assertEquals(updated, event.eventInvitees());
+  }
+
+  @Test
+  public void testInvalidEventModificationRemoveInviteeDoesntExist() {
+    Event event = new NUEvent(new ArrayList<>(List.of("Ella")),
+            "Soccer", "Carter Field", false,
+            MONDAY, 1100, MONDAY, 1200);
+    List<String> original = new ArrayList<>(List.of("Ella"));
+    assertEquals(original, event.eventInvitees());
+    assertThrows(IllegalArgumentException.class, () -> event.removeInvitee("Abby"));
+  }
+
+  @Test
   public void testValidEventModificationAddInvitee() {
     Event event = new NUEvent(new ArrayList<>(List.of("John", "Ella")),
             "Soccer", "Carter Field", false,
@@ -343,6 +419,26 @@ public class TestEvent {
     event.addInvitee("Karina");
     List<String> updated = new ArrayList<>(List.of("John", "Ella", "Karina"));
     assertEquals(updated, event.eventInvitees());
+  }
+
+  @Test
+  public void testInvalidEventModificationAddInviteeHostAlreadyExists() {
+    Event event = new NUEvent(new ArrayList<>(List.of("Ella")),
+            "Soccer", "Carter Field", false,
+            MONDAY, 1100, MONDAY, 1200);
+    List<String> original = new ArrayList<>(List.of("Ella"));
+    assertEquals(original, event.eventInvitees());
+    assertThrows(IllegalArgumentException.class, () -> event.addInvitee("Ella"));
+  }
+
+  @Test
+  public void testInvalidEventModificationAddInviteeAlreadyExists() {
+    Event event = new NUEvent(new ArrayList<>(List.of("Ella", "Caesar")),
+            "Soccer", "Carter Field", false,
+            MONDAY, 1100, MONDAY, 1200);
+    List<String> original = new ArrayList<>(List.of("Ella", "Caesar"));
+    assertEquals(original, event.eventInvitees());
+    assertThrows(IllegalArgumentException.class, () -> event.addInvitee("Caesar"));
   }
 }
 
