b840ff6d03175cdb328117c707cb3f165da10647
Brandon <lu.bra@northeastern.edu>
2024-04-05 23:20:25 -0400

    Added SchedulingStrategies interface to work on later and implement the actual strategies.

---
+++ src/cs3500/nuplanner/strategies/SchedulingStrategies.java
@@ -0,0 +1,18 @@
+package cs3500.nuplanner.strategies;
+
+import cs3500.nuplanner.model.hw05.Event;
+import cs3500.nuplanner.model.hw05.SchedulingSystem;
+
+/**
+ * Represents the scheduling strategies available to a user to determine when individuals in the
+ * system can meet and is chosen through a command line argument.
+ */
+public interface SchedulingStrategies {
+  /**
+   * Strategy will then decide when to schedule the event based on certain criteria.
+   *
+   * @param model the model passed in
+   * @param duration the duration of the event
+   */
+  Event chooseSchedulingStrategy(SchedulingSystem model, int duration);
+}
