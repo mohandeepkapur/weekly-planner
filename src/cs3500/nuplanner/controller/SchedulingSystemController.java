13334123442c59fbe11dde4d2ea31a8797753825
Brandon <lu.bra@northeastern.edu>
2024-04-08 11:44:49 -0400

    Added more description to the JavaDocs as described in the HW6 feedback.

--- src/cs3500/nuplanner/controller/SchedulingSystemController.java
+++ src/cs3500/nuplanner/controller/SchedulingSystemController.java
@@ -3,17 +3,18 @@ package cs3500.nuplanner.controller;
 import cs3500.nuplanner.model.hw05.SchedulingSystem;
 
 /**
- * Controller for Scheduling System.
+ * An interface that describes the overarching methods for the controller that run the entire
+ * program and handle user input for the Scheduling System.
  */
 public interface SchedulingSystemController {
 
   /**
-   * Runs scheduling system using user input.
+   * Launches a Scheduling System that can handle user input and is connected to the model.
    */
   void launchSchedulingSystem(SchedulingSystem model);
 
   /**
-   * Runs scheduling system using XML file.
+   * Launches a Scheduling System by loading in an XML file of already created user schedules.
    *
    * @param pathname                     path to XML file
    * @throws IllegalStateException       if unable to open or parse XML file
