47a38f1be86f3a3db101b23765f1ab0cb9df04b4
mohkapur <98370824+mohkapur@users.noreply.github.com>
2024-04-17 17:00:05 -0400

    adapt changes

--- src/cs3500/nuplanner/providerp3/adaptors/ProviderToClientViewAdaptor.java
+++ src/cs3500/nuplanner/providerp3/adaptors/ProviderToClientViewAdaptor.java
@@ -1,7 +1,57 @@
 package cs3500.nuplanner.providerp3.adaptors;
 
-public class ProviderToClientViewAdaptor {
+import cs3500.nuplanner.controller.Features;
+import cs3500.nuplanner.model.hw05.ReadableEvent;
+import cs3500.nuplanner.providerp3.view.IView;
+import cs3500.nuplanner.view.gui.SSGUIView;
 
+public class ProviderToClientViewAdaptor implements SSGUIView {
 
+  // providers don't have a MAIN view interface
+  // assuming dealing w/ MAIN impl
+  private IView delegate;
 
+  public ProviderToClientViewAdaptor(IView view) {
+    this.delegate = view;
+  }
+
+  @Override
+  public void displayUserSchedule(String user) {
+    
+  }
+
+  @Override
+  public void displayBlankEvent() {
+
+  }
+
+  @Override
+  public void displayBlankScheduleEvent() {
+
+  }
+
+  @Override
+  public void displayExistingEvent(String user, ReadableEvent event) {
+
+  }
+
+  @Override
+  public void addFeatures(Features features) {
+
+  }
+
+  @Override
+  public void makeVisible() {
+
+  }
+
+  @Override
+  public void displayErrorMessage(String errorMessage) {
+
+  }
+
+  @Override
+  public void refresh() {
+
+  }
 }
