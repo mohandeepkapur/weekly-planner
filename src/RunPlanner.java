c8d73dd1fa771666f43fd8577936d6995460c128
mohkapur <98370824+mohkapur@users.noreply.github.com>
2024-04-18 13:02:14 -0400

    docs

--- src/RunPlanner.java
+++ src/RunPlanner.java
@@ -6,6 +6,7 @@ import cs3500.nuplanner.model.hw05.SchedulingSystem;
 import cs3500.nuplanner.provider.adaptors.ClientToProviderModelAdaptor;
 import cs3500.nuplanner.provider.adaptors.ProviderToClientViewAdaptor;
 import cs3500.nuplanner.provider.model.ReadOnlySystems;
+import cs3500.nuplanner.provider.view.IView;
 import cs3500.nuplanner.provider.view.PlannerFrame;
 import cs3500.nuplanner.strategies.AnyTimeStrategy;
 import cs3500.nuplanner.strategies.SchedulingStrategies;
@@ -16,7 +17,7 @@ import cs3500.nuplanner.view.gui.SSGUIView;
 /**
  * Class to run Scheduling System.
  */
-public final class Main {
+public final class RunPlanner {
 
   /**
    * Runs Scheduling System.
@@ -24,29 +25,31 @@ public final class Main {
   public static void main(String[] args) {
 
     SchedulingSystem model = new NUPlannerModel();
-
-    // Providing model with Schedule
     SchedulingSystemController xmlCont = new XMLController(model);
     xmlCont.useSchedulingSystem("XMLFiles/toRead/Prof. Lucia.xml");
 
-    // User can choose between Provider or Default view
-    if (args.length < 2 || args[1].equals("default")) {
+    if (args[0].equals("default")) {
       SSGUIView view = new SSFrame(model);
-      SchedulingSystemController controller = new GUIController(view, getStrategy(args[0]));
-      controller.useSchedulingSystem(model, getStrategy(args[0]));
-
+      // TODO: remove Strategy input from controller constructor, it's redundant
+      SchedulingSystemController controller = new GUIController(view, getStrategy(args[1]));
+      controller.useSchedulingSystem(model, getStrategy(args[1]));
     }
 
-    if (args[1].equals("provider")) {
-      ReadOnlySystems adaptModel = new ClientToProviderModelAdaptor(model);
-      SSGUIView adaptView = new ProviderToClientViewAdaptor(new PlannerFrame(adaptModel));
-      // remove getStrategy from constructor, it is redundant, and
-      SchedulingSystemController controller = new GUIController(adaptView, getStrategy(args[0]));
-      controller.useSchedulingSystem(model, getStrategy(args[0]));
+    if (args[0].equals("provider")) {
+      ReadOnlySystems providerModel = new ClientToProviderModelAdaptor(model);
+      IView providerView = new PlannerFrame(providerModel);
+      SSGUIView clientView = new ProviderToClientViewAdaptor(providerView);
+      SchedulingSystemController controller = new GUIController(clientView, getStrategy(args[1]));
+      controller.useSchedulingSystem(model, getStrategy(args[1]));
     }
 
   }
 
+  /**
+   * Scheduling Strategy factory based on CLA.
+   * @param arg     cla
+   * @return        Scheduling Strategy
+   */
   private static SchedulingStrategies getStrategy(String arg) {
     if (arg.equals("anytime")) {
       return new AnyTimeStrategy();
